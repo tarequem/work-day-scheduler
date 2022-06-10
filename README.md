b62506d4eb2238b362eb02b3affb4977e7d42a81
tarequem <moatartareque@gmail.com>
2022-06-10 11:52:02 -0500

    fixed issue with persistence

--- README.md
+++ README.md
@@ -8,8 +8,7 @@
 ![screenshot](/snapshot.png)
 
 # About
-** NOTE: The feature for persistence is still a work in progress. **
-* This page was programmed with the intent of having the user plan their daily work schedule. On the top of the page the user can see today's date. As the work day progresses, the website will colour code which hour is present, which hours have passed, and which hours are coming up. The user can input text of what their schedule is. Soon the user should be able to reload and see their tasks saved.
+* This page was programmed with the intent of having the user plan their daily work schedule. On the top of the page the user can see today's date. As the work day progresses, the website will colour code which hour is present, which hours have passed, and which hours are coming up. The user can input text of what their schedule is. when users click the save button for each input field, they will see that their written plans will persist.
 
 # Contributors 
 * Tareque M
--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,12 @@ $(document).ready(function () {
     //function for rendering time blocks, text area, and save buttons
     for (var i = 9; i <= 17; i++){
         var hourBlock = $(
-            `<div class="d-flex flex-row time-block">
+            `<div class="d-flex flex-row time-block justify-content-center">
                 <div id="hour${i}" class="hour col-1">
                     ${i}:00
                 </div>
                 <textarea id="text-area${i}" class="form-control description col-6 textarea"></textarea>
-                <button id="save-btn" class="saveBtn col-1"></button>
+                <button id="save-btn" class="saveBtn col-1"><img src="./assets/images/image.png" /></button>
             </div>`);
         //appends div in hourBlock to container section
         $("#container").append(hourBlock);
@@ -26,7 +26,7 @@ $(document).ready(function () {
         function colourCode() {
             for (var i = 9; i <= 17; i++) {
                 
-                if (hour===i) {
+                if (hour==i) {
                     $(`#text-area${i}`).addClass("present");
                 } else if (hour > i){
                     $(`#text-area${i}`).addClass("past");
@@ -39,24 +39,26 @@ $(document).ready(function () {
     //calls colour coding function
     colourCode();
 
-    //saves values from textareas
-    $(".saveBtn.col-1").on("click", function () {
-        for (var i = 9; i <= 17; i++) {
-            var text = $(`#text-area${i}`).val();
-            var time = $(`#hour${i}`).attr("id");
-            console.log(time, text);
+    //saves values from textareas if save button is clicked
+    $(document).on({
+        "click": function () {
+            const hour = $(this).siblings(".hour.col-1").text();
+            const text = $(this).siblings(".form-control.description.col-6.textarea").val();
+            localStorage.setItem(hour, text);
 
-            var taskObj = {time, text};
+    }}, '.saveBtn')
 
-            taskArray.push(taskObj);
-            
-            //stores array into local storage
-            localStorage.setItem("taskArray", JSON.stringify(taskArray));
-        }
-    });
+    //ensures persistence of saved 
+    function loadPlanner () {
+        $(".hour.col-1").each(function() {
+            var hour = $(this).text();
+            var text = localStorage.getItem(hour);
 
-    var taskArray = []; 
+            if(text !== null) {
+                $(this).siblings(".form-control.description.col-6.textarea").val(text);
+            }
+        })
 
-    //loading feature in progress
-
+    }
+    loadPlanner();
 });
